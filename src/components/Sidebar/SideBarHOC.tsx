2d356d4dc46546dfe631492e433adc8fe8fa8850
Your Name <ashutosh@tezsure.com>
2022-06-20 18:54:47 +0530

    overlay changes with srinivas

--- src/components/Sidebar/SideBarHOC.tsx
+++ src/components/Sidebar/SideBarHOC.tsx
@@ -8,17 +8,24 @@ export interface ISideBarHOCProps {
 
 export function SideBarHOC (props: ISideBarHOCProps) {
   return (
+    <>
+    <div className='cicle_animation'>
+      <div className='circle1'></div>
+      <div className='circle2'></div>
+      <div className='circle2'></div>
+    </div>
     <div className="flex flex-no-wrap flex-col">
       <TopNavBar/>
       <div className="flex flex-no-wrap">
       <SideBar/>
-      <div  style={{marginTop:'64px',marginLeft:'240px',width: 'calc(100% - 240px)'}} >
-        <div className='overflow-y-auto overflow-x-hidden px-3 py-6 z-0'>
+      <div  style={{marginTop:'0px',marginLeft:'240px',width: 'calc(100% - 240px)'}} >
+        <div className=' overflow-x-hidden h-screen px-3 py-6 z-0 overflow-y-auto' style={{paddingTop:'64px'}}>
         {props.children}
         </div>
        
       </div>
     </div>
     </div>
+    </>
   );
 }
