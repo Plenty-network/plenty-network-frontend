2ed9a7e9f33d033fc6bcec306f48b6c865122e86
Ishwaryaa <ishwaryaa@tezsure.com>
2022-07-12 13:08:27 +0530

    feat: wallet instance added

--- src/common/wallet.ts
+++ src/common/wallet.ts
@@ -1,43 +1,42 @@
-import { BeaconWallet } from "@taquito/beacon-wallet";
-import { NetworkType } from "@airgap/beacon-sdk";
-import { TezosToolkit } from "@taquito/taquito";
-import Config from '../config/config'
+import { BeaconWallet } from '@taquito/beacon-wallet';
+import { NetworkType } from '@airgap/beacon-sdk';
+import { TezosToolkit } from '@taquito/taquito';
+import Config from '../config/config';
 
-// Import local storage
 const connectedNetwork = Config.NETWORK;
-// const rpcNode = localStorage.getItem(RPC_NODE) ?? Config.RPC_NODES[connectedNetwork];
+const name = Config.NAME;
+
 const rpcNode = Config.RPC_NODES[connectedNetwork];
 
 // Beacon Wallet instance
-const wallet = new BeaconWallet({
-  name: Config.NAME,
+export const wallet = new BeaconWallet({
+  name: name,
   preferredNetwork: connectedNetwork as NetworkType,
 });
 
 // Tezos instance
 export const tezos = new TezosToolkit(rpcNode);
-tezos.setRpcProvider(rpcNode);
 tezos.setWalletProvider(wallet);
 
 // Function to check if user wallet is already conneted
-export const CheckIfWalletConnected = async (wallet : BeaconWallet) => {
-    try {
-      const activeAccount = await wallet.client.getActiveAccount();
-      if (!activeAccount) {
-        await wallet.client.requestPermissions({
-            network: {
-              type: connectedNetwork as NetworkType,
-              rpcUrl: rpcNode,
-            },
-          })
-      }
-      return {
-        success: true,
-      };
-    } catch (error) {
-      return {
-        success: false,
-        error,
-      };
+export const CheckIfWalletConnected = async (wallet: BeaconWallet) => {
+  try {
+    const activeAccount = await wallet.client.getActiveAccount();
+    if (!activeAccount) {
+      await wallet.client.requestPermissions({
+        network: {
+          type: connectedNetwork as NetworkType,
+          rpcUrl: rpcNode,
+        },
+      });
     }
-  };
+    return {
+      success: true,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error,
+    };
+  }
+};
