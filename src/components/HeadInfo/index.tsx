e508fab75cacde5da34b59c58f613427700ef3e5
Your Name <ashutosh@tezsure.com>
2022-07-27 19:05:36 +0530

    tooltip added and how to use

--- src/components/HeadInfo/index.tsx
+++ src/components/HeadInfo/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import * as React from 'react';
 import playIcon from  '../../assets/icon/pools/playIcon.svg'
 import Tooltip from '../Tooltip/Tooltip';
+import { Position, ToolTip, TooltipType } from '../Tooltip/TooltipAdvanced';
 export interface IHeadInfoProps {
     className?:string;
 }
@@ -10,11 +11,7 @@ export default function HeadInfo (props: IHeadInfoProps) {
   return (
       <div className={`${props.className} flex gap-2 items-center border-b border-b-borderCommon py-2 bg-cardBackGround`}>
            <div className='p-2 text-f18 font-medium text-white' >Pools</div>
-            <Tooltip 
-            right
-            message={'Watch how to add liquidity, stake, and earn PLY.'}> 
             <Image src={playIcon} height={'28px'} width={'28px'} className='cursor-pointer hover:opacity-90' />
-            </Tooltip>
     </div>
    
 
