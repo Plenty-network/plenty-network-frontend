ec7d150891271428541fa724c860a769c06019ff
Your Name <ashutosh@tezsure.com>
2022-08-09 21:19:23 +0530

    removed simple data and added table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -30,9 +30,6 @@ const Table = <D extends object>({ columns, data,shortby }: { columns: Column<D>
     state: { pageIndex },
     setSortBy,
   } = useTable<D>(
-    // ? cannot solve this type error for columns 😪
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
     {
       columns,
       data,
@@ -73,16 +70,19 @@ const Table = <D extends object>({ columns, data,shortby }: { columns: Column<D>
       }])
     }
   }
-  if(true){
+
     return(
       <div>
       <table className='w-full flex flex-col gap-3'>
       <thead>
         
-      {headerGroups.map((headerGroup) => (
-      <tr className='border border-borderCommon bg-cardBackGround flex px-5 py-3 items-center rounded-t-xl	rounded-b'>       
+      {headerGroups.map((headerGroup,index) => (
+      <tr
+      key={`headerGroup_${index}`}
+      className='border border-borderCommon bg-cardBackGround flex px-5 py-3 items-center rounded-t-xl	rounded-b'>       
         {headerGroup.headers.map((column,i) => (      
            <Tabs 
+          key={`tabls_${column.render('Header')?.toString()}_${i}`}
            text={column.render('Header')?.toString()}
            className='justify-start'
            isFirstRow={i==0}
@@ -122,70 +122,6 @@ const Table = <D extends object>({ columns, data,shortby }: { columns: Column<D>
       </div>
 
     );
-}
-
-
-
-  return (
-    <>
-    <div>
-      <div className='tableouverdir'>
-        <MuiTable {...getTableProps()} className='bridgenewtable'>
-          <TableHead>
-            {headerGroups.map((headerGroup) => (
-              // eslint-disable-next-line react/jsx-key
-              <TableRow {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  // eslint-disable-next-line react/jsx-key
-                  <TableCell {...column.getHeaderProps()} sx={{
-                    borderColor:'rgba(255, 255, 255, 0.15)',
-                    display:{xs:column.hasOwnProperty('xsShow')?'table-cell':'none',sm:'table-cell'}}}>
-                    <div className="flex flex-row align-items-center" >
-                      <span className="mx-1" style={{display:'flex'}} 
-                      onClick={()=>{!column.hasOwnProperty('canNotSearch')?shortByHandler(column.id):null}}
-                      >{column.render('Header')}
-                      {shortByGroup.id==column.id?shortByGroup.desc?<KeyboardArrowDownIcon/>:<KeyboardArrowUpIcon/>:<KeyboardArrowUpIcon sx={{opacity:0}}/>}
-                      
-                      </span>
-                    </div>
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableHead>
-          <TableBody sx={{ fontSize: '1.15rem' }}>
-            {!data.length ? simmerLoaderScreen(10):null}
-            {data.length ? page.map((row) => {
-              prepareRow(row);
-              return (
-                // eslint-disable-next-line react/jsx-key
-                <TableRow {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    return (
-                      // eslint-disable-next-line react/jsx-key
-                      <TableCell {...cell.getCellProps()} sx={{borderColor:'rgba(255, 255, 255, 0.15)',display:{xs:cell.column.hasOwnProperty('xsShow')?'table-cell':'none',sm:'table-cell'}}}>
-                        <span className="mx-1" style={{width:'100px'}}>{cell.render('Cell')}</span>
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            }):null}
-          </TableBody>
-        </MuiTable>
-        </div>
-        <Box className='paginationcontainer'>
-          <TablePagination
-            count={pageCount}
-            rowsPerPage={10}
-            page={pageIndex}
-            setPageSize={setPageSize}
-            onChangePage={(number) => gotoPage(number)}
-          />
-        </Box>
-      </div>
-    </>
-  );
 };
 
 export default Table;
