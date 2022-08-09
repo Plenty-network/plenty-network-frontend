9841fe0d300268f31dd14d36a27505f3f8c9e120
Your Name <ashutosh@tezsure.com>
2022-08-09 15:21:07 +0530

    rewards added

--- src/components/Pools/Component/CircularImageInfo.tsx
+++ src/components/Pools/Component/CircularImageInfo.tsx
@@ -4,11 +4,13 @@ import * as React from "react";
 
 export interface ICircularImageInfoProps {
   imageArray?: Array<any>;
+  className?:string;
 }
 
 export function CircularImageInfo(props: ICircularImageInfoProps) {
+  const { className='' } =props;
   return (
-    <div className="pl-1 flex flex-row" >
+    <div className={`pl-1 flex flex-row ${className}`} >
      {props.imageArray?.map((token,i)=> <ImageCircle key={`CircularImageInfo_${i}`} src={token} />)}
     </div>
   );
@@ -18,11 +20,13 @@ export interface IImageCircleProps {
   src?: any;
   width?: string;
   height?: string;
+  className?:string;
 }
 
 export function ImageCircle(props: IImageCircleProps) {
+  const {className=''} =props;
   return (
-    <span className="w-[28px] h-[28px] -ml-1 ">
+    <span className={`w-[28px] h-[28px] -ml-1 ${className}`}>
       <Image src={props.src} width={"28px"} height={"28px"} />
     </span>
   );
