6ceeaa3feae2e50b6cdd0ad90bcb952312b91e23
Your Name <ashutosh@tezsure.com>
2022-08-12 17:08:22 +0530

    address field added

--- src/hooks/usePoolsTableFilter.ts
+++ src/hooks/usePoolsTableFilter.ts
@@ -1,16 +1,22 @@
+import { usePoolsMainwithAddress } from './../api/pools/query/poolsmain.query';
 import { usePoolsMain2 } from "../api/pools/query/poolsmain.query";
 
 export const usePoolsTableFilter=(filterText:string | 'MyPools' | undefined ,address:string | undefined )=>{
-    const  { data:poolTableData=[],isFetched=false }=usePoolsMain2(); 
+    if(filterText === 'MyPools' && address){
+    const  { data:poolTableData=[],isFetched }=usePoolsMainwithAddress(address);  
+    if(poolTableData.length)
+        return {data:poolTableData,isFetched:isFetched} 
+    }
+    else{
+    const  { data:poolTableData=[],isFetched }=usePoolsMain2();  
     if(poolTableData.length){
-        if(filterText === 'MyPools'){
-            return {data:poolTableData,isFetched} 
-        }
     if(filterText){         
        const newpoolTableData= poolTableData.filter((e)=>e.poolType===filterText);
-       return {data:newpoolTableData,isFetched}
+       return {data:newpoolTableData,isFetched:isFetched}
     }
-    return {data:poolTableData,isFetched}
+    return {data:poolTableData,isFetched:isFetched}
     }
-   return {data:poolTableData,isFetched}
+   return {data:poolTableData,isFetched:isFetched}
+    }
+return {data:[],isFetched:false}
 }
