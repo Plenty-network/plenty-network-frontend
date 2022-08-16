ff9c89db9a2ba4e127cd2009abefddc0758b0e65
Ishwaryaa <ishwaryaa@tezsure.com>
2022-08-16 20:56:34 +0530

    feat: connect wallet issue fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { SideBarHOC } from '../src/components/Sidebar/SideBarHOC';
 import { AppDispatch, useAppSelector } from '../src/redux';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { fetchWallet, walletConnection } from '../src/redux/wallet/wallet';
+import { fetchWallet } from '../src/redux/wallet/wallet';
 import { getConfig } from '../src/redux/config/config';
 import { getTokenPrice } from '../src/redux/tokenPrice/tokenPrice';
 import { getTotalVotingPower } from '../src/redux/pools';
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
   useEffect(() => {
     dispatch(fetchWallet());
     dispatch(getConfig());
-    dispatch(getTotalVotingPower());
+    //dispatch(getTotalVotingPower());
   }, []);
   useEffect(() => {
     Object.keys(token).length !== 0 && dispatch(getTokenPrice());
