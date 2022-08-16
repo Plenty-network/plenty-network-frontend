ddba255a6e154ab90937a8c0c7f44218d204241d
Ishwaryaa <ishwaryaa@tezsure.com>
2022-08-16 19:34:48 +0530

    feat: tweaks on liq

--- src/components/Pools/ConfirmStaking.tsx
+++ src/components/Pools/ConfirmStaking.tsx
@@ -5,6 +5,8 @@ import info from '../../../src/assets/icon/common/infoIcon.svg';
 import Button from '../Button/Button';
 import { tokenParameterLiquidity } from '../Liquidity/types';
 import { Dropdown } from '../DropDown/Dropdown';
+import { VePLY } from '../DropDown/vePLYDropdown';
+import { IVePLYData } from '../../api/stake/types';
 
 interface IConfirmStakeLiquidity {
   tokenIn: tokenParameterLiquidity;
@@ -12,8 +14,10 @@ interface IConfirmStakeLiquidity {
   tokenOut: tokenParameterLiquidity;
   setScreen: React.Dispatch<React.SetStateAction<string>>;
   handleOperation: () => void;
-  setSelectedDropDown: React.Dispatch<React.SetStateAction<string>>;
-  selectedDropDown: string;
+  setSelectedDropDown: React.Dispatch<React.SetStateAction<IVePLYData>>;
+  selectedDropDown: IVePLYData;
+
+  vePLYOptions: IVePLYData[];
 }
 
 export function ConfirmStakeLiquidity(props: IConfirmStakeLiquidity) {
@@ -35,8 +39,8 @@ export function ConfirmStakeLiquidity(props: IConfirmStakeLiquidity) {
           </div>
           <div className="ml-auto">
             {' '}
-            <Dropdown
-              Options={['one', 'two', 'three']}
+            <VePLY
+              Options={props.vePLYOptions}
               selectedText={props.selectedDropDown}
               onClick={props.setSelectedDropDown}
             />
--- src/components/Pools/StakingScreen.tsx
+++ src/components/Pools/StakingScreen.tsx
@@ -20,6 +20,8 @@ import { Dropdown } from '../DropDown/Dropdown';
 import { AppDispatch, store } from '../../redux';
 import { useDispatch } from 'react-redux';
 import { walletConnection } from '../../redux/wallet/wallet';
+import { IVePLYData } from '../../api/stake/types';
+import { VePLY } from '../DropDown/vePLYDropdown';
 
 export enum StakingScreenType {
   Staking = 'Staking',
@@ -40,8 +42,9 @@ export interface IStakingScreenProps {
   setScreen: React.Dispatch<React.SetStateAction<string>>;
   stakedToken: string;
   setStakingScreen: React.Dispatch<React.SetStateAction<StakingScreenType>>;
-  setSelectedDropDown: React.Dispatch<React.SetStateAction<string>>;
-  selectedDropDown: string;
+  setSelectedDropDown: React.Dispatch<React.SetStateAction<IVePLYData>>;
+  selectedDropDown: IVePLYData;
+  vePLYOptions: IVePLYData[];
 }
 export interface IStakingProps {
   setStakingScreen: Function;
@@ -54,8 +57,9 @@ export interface IStakingProps {
   setStakeInput: React.Dispatch<React.SetStateAction<string | number>>;
   setScreen: React.Dispatch<React.SetStateAction<string>>;
   stakedToken: string;
-  setSelectedDropDown: React.Dispatch<React.SetStateAction<string>>;
-  selectedDropDown: string;
+  setSelectedDropDown: React.Dispatch<React.SetStateAction<IVePLYData>>;
+  selectedDropDown: IVePLYData;
+  vePLYOptions: IVePLYData[];
 }
 
 export interface IUnstakingProps {
@@ -87,6 +91,7 @@ export function StakingScreen(props: IStakingScreenProps) {
           stakingScreen={props.stakingScreen}
           setSelectedDropDown={props.setSelectedDropDown}
           selectedDropDown={props.selectedDropDown}
+          vePLYOptions={props.vePLYOptions}
         />
       )}
       {props.stakingScreen === StakingScreenType.Unstaking && (
@@ -108,6 +113,7 @@ export function StakingScreen(props: IStakingScreenProps) {
 }
 
 export function Staking(props: IStakingProps) {
+  console.log(props);
   const walletAddress = store.getState().wallet.address;
   const handleInputPercentage = (value: number) => {
     props.setStakeInput(value * Number(props.pnlpBalance));
@@ -124,7 +130,7 @@ export function Staking(props: IStakingProps) {
   const onClickAmount = () => {
     handleStakeInput(props.pnlpBalance);
   };
-
+  console.log(Number(props.stakedToken));
   const dispatch = useDispatch<AppDispatch>();
   const connectTempleWallet = () => {
     return dispatch(walletConnection());
@@ -186,8 +192,8 @@ export function Staking(props: IStakingProps) {
         </div>
         {/* dropDown And InfoTab */}
         <div className="flex py-2 px-2 md:px-4 justify-between">
-          <Dropdown
-            Options={['one', 'two', 'three']}
+          <VePLY
+            Options={props.vePLYOptions}
             selectedText={props.selectedDropDown}
             onClick={props.setSelectedDropDown}
           />
