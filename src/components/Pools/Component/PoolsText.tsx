1e32f4c4b6f3a26d436acc320f5703d8a683c1d0
Your Name <ashutosh@tezsure.com>
2022-08-16 15:44:40 +0530

    tez or ctez to upper case

--- src/components/Pools/Component/PoolsText.tsx
+++ src/components/Pools/Component/PoolsText.tsx
@@ -20,16 +20,17 @@ export function PoolsText (props: IPoolsTextProps) {
   );
 }
 export function PoolsTextWithTooltip (props: IPoolsTextWithTooltipProps) {
+  const isTEZorCTEZ=(a:string)=> a === 'tez' || a === 'ctez';
     return (
       <ToolTip
       position={Position.top}
       toolTipChild={<div>
           <div className='text-text-500 text-f14 font-normal flex gap-1' >
-              <div className='text-white font-medium pr-1'>{props.token1}</div>
+              <div className={`text-white font-medium pr-1 ${isTEZorCTEZ(props.token1Name) ?'uppercase':''}`}>{props.token1}</div>
               <div>{props.token1Name}</div>
           </div>
           <div className='text-text-500 text-f14 font-normal flex gap-1' >
-              <div className='text-white font-medium pr-1'>{props.token2}</div>
+              <div className={`text-white font-medium pr-1 ${isTEZorCTEZ(props.token2Name) ?'uppercase':''}`}>{props.token2}</div>
              <div>{props.token2Name}</div>
           </div>
       </div>}
