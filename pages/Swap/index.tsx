a2d8e4f2247a8d2a11a3df9ce0b2bfe6e6b7015d
kiran-tezsure <kiran@tezsure.com>
2022-08-23 14:25:38 +0530

    fix: total voting power fetch retry condition & view time arg update to 0

--- pages/Swap/index.tsx
+++ pages/Swap/index.tsx
@@ -37,7 +37,7 @@ const Home: NextPage = (props) => {
     }
   }, [userAddress]);
   useEffect(() => {
-    if (totalVotingPowerError) {
+    if (userAddress && totalVotingPowerError) {
       dispatch(getTotalVotingPower());
     }
   }, [totalVotingPowerError]);
