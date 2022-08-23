13a4e2d688742332b6965dc847162c9052bb4059
Your Name <ashutosh@tezsure.com>
2022-08-23 14:33:03 +0530

    RANGE ADDED

--- src/components/RangeSlider/index.tsx
+++ src/components/RangeSlider/index.tsx
@@ -8,25 +8,22 @@ export interface IRangeSliderProps {
 }
 
 export function RangeSlider (props: IRangeSliderProps) {
-    const [sliderVal,setSliderVal]=React.useState( { values: [50] })
+    const [sliderVal,setSliderVal]=React.useState(50)
   return (
+      <div className='flex gap-3'>
     <div className='flex items-center gap-[7.5px]'>
-  <Image src={minus}  />      
+  <Image src={minus} className='cursor-pointer' onClick={()=>setSliderVal((oldValue)=> oldValue-10>0?(oldValue-10)%100:0)}  />      
   <Range
         step={0.1}
         min={0}
         max={100}
-        values={sliderVal.values}
-        onChange={(values) => setSliderVal({ values })}
+        values={[sliderVal]}
+        onChange={(values) => setSliderVal(values[0])}
         renderTrack={({ props, children }) => (
           <div
             {...props}
-            className='flex-1'
-            style={{
-                width:'96px',
-                backgroundColor:'#D8DAEB'
-            }}
-
+            className='flex-1 w-[96px] h-[2px] bg-text-255'
+            style={{backgroundColor:'#D8DAEB'}}
           >
             {children}
           </div>
@@ -34,11 +31,15 @@ export function RangeSlider (props: IRangeSliderProps) {
         renderThumb={({ props }) => (
           <div
             {...props}
-            className='bg-primary-500 h-1 w-2'
+            className='bg-primary-500 h-3 w-3 rounded-full border-2 border-white'
           />
         )}
       />
-  <Image src={plus}  />
+  <Image src={plus} className='cursor-pointer' onClick={()=>setSliderVal((oldValue)=> oldValue+10<100?(oldValue+10):100)}  />
+</div>
+<div className='bg-primary-500/10 border border-primary-500 py-[9px] text-center h-[38px] w-[48px] rounded-lg text-f12 '>
+{sliderVal.toFixed(0)}%
+</div>
 </div>
   );
 }
