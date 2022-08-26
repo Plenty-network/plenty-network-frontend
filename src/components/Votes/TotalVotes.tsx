3255ea879cfcac75e68fde186f63ca8483956fc9
Ishwaryaa <ishwaryaa@tezsure.com>
2022-08-26 13:49:53 +0530

    feat: vote data integration

--- src/components/Votes/TotalVotes.tsx
+++ src/components/Votes/TotalVotes.tsx
@@ -2,14 +2,18 @@ import * as React from "react";
 
 export interface IShortCardListProps {
   className?: string;
+  totalvotes: number;
+  totalVotesPercentage: number;
 }
 
 export function TotalVotes(props: IShortCardListProps) {
   return (
     <>
       <div className="flex-1 text-right  justify-center items-center">
-        <div className="font-f13 text-text-50">130k</div>
-        <div className="font-subtitle4 relative top-[10px]">37.48%</div>
+        <div className="font-f13 text-text-50">{props.totalvotes}k</div>
+        <div className="font-subtitle4 relative top-[10px]">
+          {props.totalVotesPercentage.toFixed(2)}%
+        </div>
       </div>
     </>
   );
