6f2a171589ab9edf691694566424471cde434bb1
Udit-Kapoor <udit.dude01@gmail.com>
2022-09-19 23:47:41 +0530

    faet : claim All Fees

--- src/operations/fees.ts
+++ src/operations/fees.ts
@@ -1,3 +1,4 @@
+import { OpKind } from "@taquito/taquito";
 import { dappClient, feeDistributorAddress} from "../common/walletconnect";
 import {
   IOperationsResponse,
@@ -52,3 +53,53 @@ export const claimFees = async (
     };
   }
 };
+
+export const claimAllFees = async (
+    id : number,
+    address : string,
+    amms : string[],
+    epochVoteShare : IEpochVoteShare[] ,
+    transactionSubmitModal: TTransactionSubmitModal,
+    resetAllValues: TResetAllValues,
+    setShowConfirmTransaction: TSetShowConfirmTransaction
+  ): Promise<IOperationsResponse> => {
+    try {
+      const { CheckIfWalletConnected } = dappClient();
+      const WALLET_RESP = await CheckIfWalletConnected();
+      if (!WALLET_RESP.success) {
+        throw new Error("Wallet connection failed");
+      }
+    
+      
+      const Tezos = await dappClient().tezos();
+      const feeDistributorInstance: any = await Tezos.contract.at(feeDistributorAddress);
+
+      const feesBatch : any = [];
+        for (const amm of amms) {
+          feesBatch.push({
+            kind: OpKind.TRANSACTION,
+            ...feeDistributorInstance.methods.claim(id, address , amm, epochVoteShare).toTransferParams(),
+          });
+        }
+      const batch =  Tezos.wallet.batch(feesBatch);    
+  
+      const batchOp = await batch.send();
+      setShowConfirmTransaction(false);
+      resetAllValues();
+  
+      transactionSubmitModal(batchOp.opHash);
+  
+      await batchOp.confirmation();
+      return {
+        success: true,
+        operationId: batchOp.opHash,
+      };
+    } catch (error: any) {
+      console.error(error);
+      return {
+        success: false,
+        operationId: undefined,
+        error: error.message,
+      };
+    }
+  };
