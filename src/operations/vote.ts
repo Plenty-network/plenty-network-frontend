d5db45bea00011bd22589b4ac2d9a7757bc394ba
Udit-Kapoor <udit.dude01@gmail.com>
2022-09-22 01:25:36 +0530

    feat: update operation for my portfolio

--- src/operations/vote.ts
+++ src/operations/vote.ts
@@ -78,8 +78,6 @@ export const vote = async (
       const voterInstance: any = await Tezos.contract.at(voterAddress);
 
       let batch = null;
-    
-    // TODO : Confirm the order of sending params
 
       batch = Tezos.wallet
         .batch()
@@ -91,6 +89,56 @@ export const vote = async (
             bribeId
           )
         );
+
+        const batchOp = await batch.send();
+        setShowConfirmTransaction(false);
+        resetAllValues();
+  
+        transactionSubmitModal(batchOp.opHash);
+  
+        await batchOp.confirmation();
+        return {
+          success: true,
+          operationId: batchOp.opHash,
+        };
+      
+    } catch (error : any) {
+      console.error(error);
+      return {
+        success: false,
+        operationId: undefined,
+        error : error.message,
+      };
+    }
+  };
+
+  export const claimAllBribe = async (
+    bribeData : {tokenId : number,epoch : number,bribeId : number,amm : string}[],
+    transactionSubmitModal: TTransactionSubmitModal,
+    resetAllValues: TResetAllValues,
+    setShowConfirmTransaction: TSetShowConfirmTransaction
+  ): Promise<IOperationsResponse> => {
+    try {
+      const {CheckIfWalletConnected}=dappClient()
+      const WALLET_RESP = await CheckIfWalletConnected();
+      if (!WALLET_RESP.success) {
+        throw new Error('Wallet connection failed');
+      }
+  
+      const Tezos = await dappClient().tezos();
+      const voterInstance: any = await Tezos.contract.at(voterAddress);
+
+
+      const bribeBatch : any = [];
+        for (const bribe of bribeData) {
+          bribeBatch.push({
+            kind: OpKind.TRANSACTION,
+            ...voterInstance.methods.claim_bribe(bribe.tokenId,bribe.amm,bribe.epoch,bribe.bribeId
+            ).toTransferParams(),
+          });
+        }
+      const batch =  Tezos.wallet.batch(bribeBatch);
+
 
         const batchOp = await batch.send();
         setShowConfirmTransaction(false);
