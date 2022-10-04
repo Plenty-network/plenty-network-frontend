01effd024ade71e31f9dc9bff686fec57ef3584a
kiran-tezsure <kiran@tezsure.com>
2022-10-04 19:00:04 +0530

    feat: add optimised max batch calculation function

--- src/api/util/operations.ts
+++ src/api/util/operations.ts
@@ -2,6 +2,8 @@ import { ParamsWithKind } from "@taquito/taquito";
 import { WalletParamsWithKind } from "@taquito/taquito/dist/types/wallet";
 import cloneDeep from "lodash-es/cloneDeep";
 import slice from "lodash-es/slice";
+import concat from "lodash-es/concat";
+import findLastIndex from "lodash-es/findLastIndex"
 import { dappClient } from "../../common/walletconnect";
 
 export const getMaxPossibleBatchArray = async (
@@ -40,6 +42,69 @@ export const getMaxPossibleBatchArray = async (
       }
     }
     // console.log(gasConsumed);
+    return maxPossibleBatch as WalletParamsWithKind[];
+  } catch (error: any) {
+    console.log(error);
+    throw new Error(error.message);
+  }
+};
+
+
+export const getMaxPossibleBatchArrayV2 = async (
+  operationsBatch: WalletParamsWithKind[]
+): Promise<WalletParamsWithKind[]> => {
+  try {
+    let maxPossibleBatch: ParamsWithKind[] = cloneDeep(operationsBatch) as ParamsWithKind[];
+    // maxPossibleBatch.shift(); //TODO: remove this after tzkt issue is resolved.
+    let leftoverBatch: ParamsWithKind[] = [];
+    const listOfAllBatches: ParamsWithKind[][] = [];
+    const listOfLeftoverBatches: ParamsWithKind[][] = [];
+    const Tezos = await dappClient().tezos();
+    // Add complete list as first possibility
+    listOfAllBatches.push(maxPossibleBatch);
+    listOfLeftoverBatches.push([]);
+    // Create the approx max possible batches list
+    while (maxPossibleBatch.length > 0) {
+      const midIndex = Math.floor(maxPossibleBatch.length / 2);
+      listOfLeftoverBatches.push(slice(maxPossibleBatch, midIndex, maxPossibleBatch.length));
+      listOfAllBatches.push(slice(maxPossibleBatch, 0, midIndex));
+      maxPossibleBatch = slice(maxPossibleBatch, 0, midIndex);
+    }
+
+    const promisesResult = await Promise.allSettled(
+      listOfAllBatches.map((batch) => Tezos.estimate.batch(batch))
+    );
+    // Find the first(largest) successful batch
+    const maxPossibleBatchIndex = promisesResult.findIndex(
+      (result) => result.status === "fulfilled"
+    );
+    if (maxPossibleBatchIndex < 0) {
+      throw new Error("Couldn't find successful batch operations possible.");
+    }
+    
+    const maxPossibleBatchesWithDust = [];
+    maxPossibleBatch = listOfAllBatches[maxPossibleBatchIndex];
+    leftoverBatch = listOfLeftoverBatches[maxPossibleBatchIndex];
+
+    // Add elements of leftover array(dust) one by one to maximise the possibility
+    for (let i = 0; i < leftoverBatch.length; i++) {
+      maxPossibleBatchesWithDust.push(concat(maxPossibleBatch, slice(leftoverBatch, 0, i + 1)));
+    }
+    
+    const maxPromisesReult = await Promise.allSettled(
+      maxPossibleBatchesWithDust.map((batch) => Tezos.estimate.batch(batch))
+    );
+    // Find the last(largest) successful batch
+    const finalMaxPossibleBatchIndex = findLastIndex(
+      maxPromisesReult,
+      (result) => result.status === "fulfilled"
+    );
+    
+    maxPossibleBatch =
+      finalMaxPossibleBatchIndex >= 0
+        ? maxPossibleBatchesWithDust[finalMaxPossibleBatchIndex]
+        : maxPossibleBatch;
+
     return maxPossibleBatch as WalletParamsWithKind[];
   } catch (error: any) {
     console.log(error);
