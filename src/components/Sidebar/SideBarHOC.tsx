088e84f5b2937c1408699a0276ce987afb17dd8b
Your Name <ashutosh@tezsure.com>
2022-10-06 23:56:45 +0530

    side nav fixed and video fix

--- src/components/Sidebar/SideBarHOC.tsx
+++ src/components/Sidebar/SideBarHOC.tsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from 'react';
-import { isMobile,isTablet } from 'react-device-detect';
+import React, { useState } from 'react';
+import { isMobile } from 'react-device-detect';
 import { FlashMessageHOC } from '../FlashScreen/FlashMessageHOC';
 import { NotificationBar } from '../Notification';
 import BottomNavigationBar from './BottomNavBar';
@@ -23,25 +23,20 @@ export function SideBarHOC(props: ISideBarHOCProps) {
   };
   return (
     <>
-      {/* <div className="cicle_animation">
-        <div className="circle"></div>
-        <div className="circle2"></div>
-        <div className="circle3"></div>
-      </div> */}
       <FlashMessageHOC/>
       <div className="flex flex-no-wrap flex-col">
         {!isMobile && <TopNavBar setShowNotification={showNotificationClick} />}   
         {showNotification && <NotificationBar  onhide={()=>{setShowNotification(false)}}/>}
         <div className="flex flex-no-wrap">
           {!isMobile && <SideBar />}
-          <div className="mt-0 lg:ml-[240px] md:w-[calc(100%_-_240px)] w-full mb-12 md:static absolute h-[calc(100%_-_121px)] md:mb-0">
+          <div className={`mt-0 ${!isMobile?'md:ml-[240px] md:w-[calc(100%_-_240px)]':''} w-full mb-12 md:static absolute h-[calc(100%_-_121px)] md:mb-0`}>
             <div className={`overflow-x-hidden h-screen   z-0  ${props.makeTopBarScroll || true?'static overflow-y-auto pt-[64px]':'md:absolute fixed overflow-y-hidden top-16 !m-0  h-[calc(100%_-_121px)] md:h-[calc(100%_-_64px)] md:w-[calc(100%_-_240px)] w-full'}`}>
               {props.children}
             </div>
           </div>
         </div>
-       {(isMobile || isTablet) &&<BottomNavigationBar />}
-       {(isMobile || isTablet) && <TopNavBarMobile setShowNotification={showNotificationClick} />}
+       {(isMobile ) &&<BottomNavigationBar />}
+       {(isMobile ) && <TopNavBarMobile setShowNotification={showNotificationClick} />}
       </div>
     </>
   );
