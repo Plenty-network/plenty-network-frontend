feb89056a022553f4e9f319bc2d8ce2394362cc8
Udit-Kapoor <udit.dude01@gmail.com>
2022-10-06 21:19:21 +0530

    feat: mybribes api

---
+++ src/api/bribes/index.ts
@@ -0,0 +1,47 @@
+import { store } from '../../redux';
+import axios from 'axios';
+import Config from '../../config/config';
+import { IuserBribeData, IuserBribeDataResponse } from './types';
+
+export const userBribeData = async (address : string , tokenPrice: { [id: string]: number }) : Promise<{success : boolean , allData : { [id: string]: IuserBribeDataResponse }}> => {
+    try {
+
+        const state = store.getState();
+        const AMMS = state.config.AMMs;
+
+        const userBribeResponse = await axios.get(`${Config.VE_INDEXER}bribes-provider?address=${address}`);
+        const myBribesData : IuserBribeData[] = userBribeResponse.data;
+
+        const allData: { [id: string]: IuserBribeDataResponse } = {};
+
+        for(var bribe of myBribesData){
+            const AMM = AMMS[bribe.amm];
+
+            allData[bribe.amm]= {
+            tokenA: AMM.token1.symbol,
+            tokenB: AMM.token2.symbol,
+            poolType: AMM.type,
+
+            bribeValue : bribe.value.multipliedBy(tokenPrice[bribe.name]),
+            bribeToken : bribe.name , 
+
+            epoch : bribe.epoch
+        };
+
+        }
+
+        return{
+            success : true,
+            allData : allData
+        };
+
+        
+    } catch (error) {
+        console.log(error);
+        return{
+            success : false,
+            allData : {}
+        };
+    }
+    
+}
