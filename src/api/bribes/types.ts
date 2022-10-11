4def52dd3fe4fb40f67aa21ae0314e0a82a17ec3
kiran-tezsure <kiran@tezsure.com>
2022-10-11 20:13:42 +0530

    feat: add bribe pools data api

--- src/api/bribes/types.ts
+++ src/api/bribes/types.ts
@@ -1,5 +1,6 @@
 import { BigNumber } from "bignumber.js";
 import { AMM_TYPE } from "../../config/types";
+import { Bribes, VolumeVeData } from "../pools/types";
 
 export interface IUserBribeIndexerData {
   value: string;
@@ -21,5 +22,41 @@ export interface IUserBribeData {
 export interface IUserBribeDataResponse {
   success: boolean;
   userBribesData: IUserBribeData[];
+  error?: string;
+}
+
+export interface IPoolsDataObject {
+  [key: string]: VolumeVeData;
+}
+
+export interface IPoolsBribeLiquidityData {
+  [id: string]: {
+    bribes: BigNumber;
+    liquidity: BigNumber;
+    bribesData: Bribes[];
+    liquidityTokenA: BigNumber;
+    liquidityTokenB: BigNumber;
+  };
+}
+
+export interface IPoolsForBribesData{
+  amm: string;
+  tokenA: string;
+  tokenB: string;
+  poolType: AMM_TYPE;
+  bribes : BigNumber;
+  bribesData : Bribes[];
+  liquidity : BigNumber;
+  liquidityTokenA : BigNumber;
+  liquidityTokenB : BigNumber;
+  totalVotesCurrent : BigNumber;
+  totalVotesPercentageCurrent : BigNumber;
+  totalVotesPrevious : BigNumber;
+  totalVotesPercentagePrevious : BigNumber;
+}
+
+export interface IPoolsForBribesResponse {
+  success: boolean;
+  poolsData: IPoolsForBribesData[],
   error?: string;
 }
