fc7103b1fad9389594a8d3c706cb9eac552a7b25
Ishwaryaa <ishwaryaa@tezsure.com>
2022-10-26 17:35:53 +0530

    fix: bug fixes

--- pages/myportfolio/index.tsx
+++ pages/myportfolio/index.tsx
@@ -565,7 +565,7 @@ function MyPortfolio(props: any) {
         >
           <Image
             src={playIcon}
-            onClick={() => setShowVideoModal(true)}
+            //onClick={() => setShowVideoModal(true)}
             height={"28px"}
             width={"28px"}
             className="cursor-pointer hover:opacity-90"
