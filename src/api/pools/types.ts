0ce7fee4c35d5ce85d6776e456fe73181f1e8efa
kiran-tezsure <kiran@tezsure.com>
2022-11-09 17:37:43 +0530

    feat: add gauge availability identifier to pools api

--- src/api/pools/types.ts
+++ src/api/pools/types.ts
@@ -80,7 +80,7 @@ export interface IPoolsDataWrapperResponse {
   bribes : Bribes[];
   isLiquidityAvailable : boolean;
   isStakeAvailable: boolean;
-
+  isGaugeAvailable: boolean;
 }
 
 export interface IAnalyticsDataObject {
