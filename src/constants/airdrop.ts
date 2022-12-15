68528f3c904c6277b8e5e1e7c56761ac12e4c611
kiran-tezsure <kiran@tezsure.com>
2022-12-15 14:21:18 +0530

    feat: add airdrop missions list for ui

--- src/constants/airdrop.ts
+++ src/constants/airdrop.ts
@@ -1,3 +1,4 @@
+import { Mission } from "../api/airdrop/types";
 import { EvmCTAState } from "../redux/airdrop/types";
 
 export const AIRDROP_ERROR_MESSAGES = {
@@ -18,5 +19,32 @@ export const AIRDROP_EVM_CTA_TEXTS: { [key in EvmCTAState]: string } = {
   [EvmCTAState.NOT_SIGNED]: "Confirm Tezos address",
   [EvmCTAState.NOT_REVEALED]: "Reveal Tezos address",
   [EvmCTAState.WRONG_NETWORK]: "Switch network",
-  [EvmCTAState.LOADING]: "Loading..."
+  [EvmCTAState.LOADING]: "Loading...",
 };
+
+export const AIRDROP_MISSIONS_FOR_DISPLAY: { mission: Mission; displayText: string }[] = [
+  {
+    mission: Mission.ELIGIBLE,
+    displayText: "Tweet",
+  },
+  {
+    mission: Mission.TRADE,
+    displayText: "Make a trade on Plenty",
+  },
+  {
+    mission: Mission.LP,
+    displayText: "Add Liquidity for any pair",
+  },
+  {
+    mission: Mission.STAKE,
+    displayText: "Stake the liquidity position in a gauge",
+  },
+  {
+    mission: Mission.LOCK,
+    displayText: "Lock PLY as veNFT",
+  },
+  {
+    mission: Mission.VOTE,
+    displayText: "Vote for any pair in the guages",
+  },
+];
