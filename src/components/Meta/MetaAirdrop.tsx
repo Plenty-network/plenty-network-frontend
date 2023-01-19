53fdea0a01399ff0988a18e7b9d561df0b3f67a8
Ishwaryaa <ishwaryaa@tezsure.com>
2023-01-19 12:01:01 +0530

    feat: twitter og

--- src/components/Meta/MetaAirdrop.tsx
+++ src/components/Meta/MetaAirdrop.tsx
@@ -20,7 +20,10 @@ export function MetaAirdrop() {
         content="The biggest airdrop on the Tezos blockchain is now live! Largest DeFi protocol of Tezos, Plenty Network is doing an airdrop on the occasion of it's highly anticipated platform relaunch. If you have ever used Quickswap, LooksRare, Pancakeswap, Velodrome and popular Tezos Dapps, then you might be eligible for the PLY airdrop. Claim your PLY today and start earning on the Plenty Network by locking and voting."
         property="twitter:description"
       />
-      <meta content="/assets/airdropOG.png" property="twitter:image" />
+      <meta
+        content="https://uploads-ssl.webflow.com/6307b856943c0f1358714dab/63c8e289fd47ec5d358a87b8_airdrop-OG.png"
+        property="twitter:image"
+      />
       <meta property="og:type" content="website" />
       <meta content="summary_large_image" name="twitter:card" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
