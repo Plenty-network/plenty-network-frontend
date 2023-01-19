c5a6e3961645c28f4d4a13fcab5cffd6ffb4c373
Ishwaryaa <ishwaryaa@tezsure.com>
2023-01-19 12:17:09 +0530

    feat:meta tag removed from document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,16 +24,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="description" content={siteData.description} />
-          <meta
-            name="keywords"
-            content="plentydefi, plenty, tezos, v3, defi, plenty network, veply, PLY, v(3,3), ecosystem, mainnet, blockchain"
-          />
-          <meta property="og:title" content={siteData.title} />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content={siteData.url} />
-          <meta property="og:description" content={siteData.description} />
-
           <link rel="icon" type="image/svg+xml" href="/assets/images/favicon.svg" />
           <link rel="icon" type="image/png" href="/assets/images/favicon.png" />
           <link rel="icon" href="/favicon.ico" type="image/x-icon" />
