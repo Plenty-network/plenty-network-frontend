1f456c9b0208c62265e623c3058626321d6a6d62
kapil1221 <kapilagrawal32@gmail.com>
2023-05-16 13:25:29 +0530

    synamic config use

--- src/api/v3/liquidity.ts
+++ src/api/v3/liquidity.ts
@@ -8,16 +8,19 @@ const v3ContractAddress = `KT1M5yHd85ikngHm5YCu9gkfM2oqtbsKak8Y`;
 export const connectedNetwork = Config.NETWORK;
 
 const TokenDetail = async(tokenSymbol : String) : Promise<Token> => {
-    const configResponse = await axios.get(`${Config.ANALYTICS_INDEXER[connectedNetwork]}analytics/tokens/${tokenSymbol}`);
-    let tokenAddress = configResponse.data[0].contract;
-    let tokenStandard = configResponse.data[0].standard;
-    let tokenDecimals = configResponse.data[0].decimals;
+    let configResponse :any = await axios.get(Config.CONFIG_LINKS.testnet.TOKEN);
+    configResponse = configResponse.data[`${tokenSymbol}`];
+
+    let tokenAddress = configResponse.address;
+    let tokenStandard = configResponse.standard;
+    let tokenDecimals = configResponse.decimals;
 
     return {
         address: tokenAddress,
         standard: tokenStandard,
         decimals: tokenDecimals,
     }
+    
 }
 
 const ContractStorage = async(tokenXSymbol : String, tokenYSymbol : String) : Promise<any> => {
@@ -51,7 +54,8 @@ export const calculateCurrentPrice = async ( tokenXSymbol: String, tokenYSymbol:
         } else {
             currentPrice = PoolObject.getRealPriceTokenX();
         }
-
+        
+        console.log('v3', currentPrice)
         return currentPrice;
     }
     catch(error) {
@@ -65,7 +69,8 @@ export const calculateFullRange = async ( tokenXSymbol: String, tokenYSymbol: St
           let contractStorageParameters = await ContractStorage(tokenXSymbol, tokenYSymbol)
           let PoolObject = new Pool(contractStorageParameters.tokenX, contractStorageParameters.tokenY, contractStorageParameters.currTickIndex, contractStorageParameters.tickSpacing, contractStorageParameters.sqrtPriceValue);
           let tickFullRange = PoolObject.getFullRangeBoundaries();
-
+            
+          console.log("v3----", tickFullRange);
           return tickFullRange;
       }
       catch(error) {
@@ -89,6 +94,8 @@ export const calculateMinandMaxPriceFromTick = async (tokenXSymbol: String, toke
           console.log('v3-------v3', minTick, maxTick, minPriceValue, maxPriceValue);
 
           return {
+            minTick : minTick,
+            maxTick : maxTick,
             minValue : minPriceValue,
             maxValue : minPriceValue,
           }
@@ -132,6 +139,19 @@ export const estimateTokenBFromTokenA = async ( amount: BigNumber, tokenXSymbol:
           estimatedAmount = PoolObject.estimateAmountYFromX(amount, lowerTickIndex, upperTickIndex);
 
           return estimatedAmount;
+      }
+      catch(error) {
+          console.log("v3 error: ", error);
+      }
+}
+
+export const calculateNearTickSpacing = async ( tick: number, space: number
+    ): Promise<any>  => {
+      try {
+          let nearestTick = Tick.nearestUsableTick(tick, space);
+            
+          console.log("v3----", nearestTick);
+          return nearestTick;
       }
       catch(error) {
           console.log("v3 error: ", error);
