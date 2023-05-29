60c15eb0c5c0243641f6b4932cc4807213823f77
kapil1221 <kapilagrawal32@gmail.com>
2023-05-29 15:31:59 +0530

    added calsulate tick from real price

--- src/api/v3/helper.ts
+++ src/api/v3/helper.ts
@@ -103,6 +103,17 @@ export const getRealPriceFromTick = async (tick: number, tokenXSymbol: string, t
   }
 };
 
+export const getTickFromRealPrice = async (realPrice: BigNumber, tokenXSymbol: string, tokenYSymbol: string): Promise<any> => {
+  try {
+    let contractStorageParameters = await ContractStorage(tokenXSymbol, tokenYSymbol);
+    let tick = Tick.computeTickFromRealPrice(realPrice, contractStorageParameters.tokenX, contractStorageParameters.tokenY, contractStorageParameters.tickSpacing);
+
+    return tick;
+  } catch (error) {
+    console.log("v3 error: ", error);
+  }
+};
+
 export const calculateliquidity = async (
   amount: BalanceNat,
   lowerTick: number,
