b1c375f19b2a00c7c344fb340370e2e7ed9d2f88
Ishwaryaa <ishwaryaa@tezsure.com>
2023-05-31 18:50:46 +0530

    feat: expected rewards column added

--- src/components/Votes/VotesTable.tsx
+++ src/components/Votes/VotesTable.tsx
@@ -193,8 +193,7 @@ export function VotesTable(props: IVotesTableProps) {
     ],
     [valueFormat]
   );
-
-  const desktopcolumns = React.useMemo<Column<IVotePageData>[]>(
+  const desktopcolumnsNFT = React.useMemo<Column<IVotePageData>[]>(
     () => [
       {
         Header: "Pools",
@@ -302,6 +301,21 @@ export function VotesTable(props: IVotesTableProps) {
         ),
       },
       {
+        Header: "Max rewards",
+        id: "Max rewards",
+        columnWidth: "w-[112px] pr-2.5",
+        tooltipMessage: "Number of votes given through the selected veNFT to this pool.",
+        isToolTipEnabled: true,
+        sortType: (a: any, b: any) => compareNumericString(a, b, "votes.expectedRewards"),
+        canShort: true,
+
+        accessor: (x: any) => (
+          <span className="font-subtitle4">
+            {props.selectedDropDown.tokenId === "" ? "-" : `${x.votes.expectedRewards.toFixed(2)}`}{" "}
+          </span>
+        ),
+      },
+      {
         Header: "My votes",
         id: "Myvotes",
         columnWidth: "w-[112px] pr-2.5",
@@ -314,6 +328,161 @@ export function VotesTable(props: IVotesTableProps) {
           <MyVotesValue myVotes={x.votes.myVotes} myVotesPercentage={x.votes.myVotesPercentage} />
         ),
       },
+
+      {
+        Header: "Votes",
+        id: "Myvotess",
+        columnWidth: "w-[237px] ml-auto",
+        canShort: true,
+        sortType: (a: any, b: any) => compareNumericString(a, b, "votes.totalVotes"),
+
+        accessor: (x: any) => (
+          <MyVotes
+            isMobile={false}
+            tokenA={tEZorCTEZtoUppercase(x.votes.tokenA.toString())}
+            tokenB={tEZorCTEZtoUppercase(x.votes.tokenB.toString())}
+            setSelectedPools={props.setSelectedPools}
+            selectedPools={props.selectedPools}
+            setTotalVotingPower={props.setTotalVotingPower}
+            totalVotingPower={props.totalVotingPower}
+            amm={x.amm}
+            votes={props.votes}
+            setVotes={props.setVotes}
+            selectedDropDown={props.selectedDropDown}
+            totalVotesPercentage={Number(x.votes.myVotesPercentage)}
+            isCurrentEpoch={props.isCurrentEpoch}
+            index={x.index}
+            votedata={votedata}
+            totalVotes1={totalVotes1}
+            sumOfVotes={props.sumOfVotes}
+          />
+        ),
+      },
+    ],
+    [valueFormat]
+  );
+  const desktopcolumns = React.useMemo<Column<IVotePageData>[]>(
+    () => [
+      {
+        Header: "Pools",
+        id: "pools",
+        showOnMobile: true,
+        columnWidth: "w-[190px]",
+        canShort: true,
+        sortType: (a: any, b: any) => compareNumericString(a, b, "votes.tokenA", true),
+        accessor: (x: any) => (
+          <div className=" flex justify-center items-center">
+            <div className="bg-card-600 rounded-full w-[28px] h-[28px] flex justify-center items-center overflow-hidden">
+              <img
+                alt={"alt"}
+                src={
+                  tEZorCTEZtoUppercase(x.votes.tokenA) === "CTEZ"
+                    ? tokenIcons[x.votes.tokenB]
+                      ? tokenIcons[x.votes.tokenB].src
+                      : tokens[x.votes.tokenB]?.iconUrl
+                      ? tokens[x.votes.tokenB].iconUrl
+                      : `/assets/Tokens/fallback.png`
+                    : tokenIcons[x.votes.tokenA]
+                    ? tokenIcons[x.votes.tokenA].src
+                    : tokens[x.votes.tokenA]?.iconUrl
+                    ? tokens[x.votes.tokenA].iconUrl
+                    : `/assets/Tokens/fallback.png`
+                }
+                width={"24px"}
+                height={"24px"}
+                onError={changeSource}
+              />
+            </div>
+            <div className="w-[28px] relative -left-2 bg-card-600 rounded-full h-[28px] flex justify-center items-center overflow-hidden">
+              <img
+                alt={"alt"}
+                src={
+                  tEZorCTEZtoUppercase(x.votes.tokenA) === "CTEZ"
+                    ? tokenIcons[x.votes.tokenA]
+                      ? tokenIcons[x.votes.tokenA].src
+                      : tokens[x.votes.tokenA]?.iconUrl
+                      ? tokens[x.votes.tokenA].iconUrl
+                      : `/assets/Tokens/fallback.png`
+                    : tokenIcons[x.votes.tokenB]
+                    ? tokenIcons[x.votes.tokenB].src
+                    : tokens[x.votes.tokenB]?.iconUrl
+                    ? tokens[x.votes.tokenB].iconUrl
+                    : `/assets/Tokens/fallback.png`
+                }
+                width={"24px"}
+                height={"24px"}
+                onError={changeSource}
+              />
+            </div>
+            <div>
+              <div className="font-body4">
+                {" "}
+                {tEZorCTEZtoUppercase(x.votes.tokenA) === "CTEZ"
+                  ? ` ${tEZorCTEZtoUppercase(x.votes.tokenB?.toString())} / ${tEZorCTEZtoUppercase(
+                      x.votes.tokenA?.toString()
+                    )}`
+                  : ` ${tEZorCTEZtoUppercase(x.votes.tokenA?.toString())} / ${tEZorCTEZtoUppercase(
+                      x.votes.tokenB?.toString()
+                    )}`}
+              </div>
+              <div className="font-subtitle1 text-text-500">{x.votes.poolType} Pool</div>
+            </div>
+          </div>
+        ),
+      },
+      {
+        Header: "Rewards",
+        id: "Rewards",
+        columnWidth: "w-[112px]",
+        tooltipMessage:
+          "Trading fees and bribes to be distributed across the voters of this pool. The reward may increase as the epoch progresses.",
+        isToolTipEnabled: true,
+        canShort: true,
+        showOnMobile: true,
+        sortType: (a: any, b: any) =>
+          compareNumericStringRewards(a, b, "votes.bribes", "votes.fees"),
+        accessor: (x: any) => (
+          <RewardsData
+            bribes={x.votes.bribes}
+            fees={x.votes.fees}
+            token1Name={tEZorCTEZtoUppercase(x.votes.tokenA.toString())}
+            token2Name={tEZorCTEZtoUppercase(x.votes.tokenB.toString())}
+            fees1={Number(x.votes.feesTokenA)}
+            fees2={Number(x.votes.feesTokenB)}
+            bribesData={x.votes.bribesData}
+          />
+        ),
+      },
+      {
+        Header: "Total votes",
+        id: "Total votes",
+        columnWidth: "w-[112px]",
+        canShort: true,
+        tooltipMessage: "Total votes received by the pool in the current epoch.",
+        isToolTipEnabled: true,
+        sortType: (a: any, b: any) => compareNumericString(a, b, "votes.totalVotesPercentage"),
+        accessor: (x: any) => (
+          <TotalVotes
+            totalvotes={x.votes.totalVotes}
+            totalVotesPercentage={x.votes.totalVotesPercentage}
+          />
+        ),
+      },
+
+      {
+        Header: "My votes",
+        id: "Myvotes",
+        columnWidth: "w-[112px] pr-2.5",
+        tooltipMessage: "Number of votes given through the selected veNFT to this pool.",
+        isToolTipEnabled: true,
+        sortType: (a: any, b: any) => compareNumericString(a, b, "votes.myVotesPercentage"),
+        canShort: true,
+
+        accessor: (x: any) => (
+          <MyVotesValue myVotes={x.votes.myVotes} myVotesPercentage={x.votes.myVotesPercentage} />
+        ),
+      },
+
       {
         Header: "Votes",
         id: "Myvotess",
@@ -350,7 +519,15 @@ export function VotesTable(props: IVotesTableProps) {
     <>
       <div className={`overflow-x-auto inner ${props.className}`}>
         <Table<any>
-          columns={isTablet ? desktopcolumns : isMobile ? mobilecolumns : desktopcolumns}
+          columns={
+            isTablet
+              ? props.sumOfVotes === 100
+                ? desktopcolumnsNFT
+                : desktopcolumns
+              : isMobile
+              ? mobilecolumns
+              : desktopcolumns
+          }
           data={votedata}
           noSearchResult={noSearchResult}
           shortby="Rewards"
